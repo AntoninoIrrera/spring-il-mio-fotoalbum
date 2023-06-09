7dc35c49ecdd5174a2ee846f61a3ed0a545590ef
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-06-09 15:26:21 +0200

    form invio commento foto

--- vue/fotoalbum/src/views/FotoShow.vue
+++ vue/fotoalbum/src/views/FotoShow.vue
@@ -10,7 +10,10 @@ export default {
     data() {
         return {
             foto: '',
+            messaggio: '',
+            errors: [],
             urlAddress: 'http://127.0.0.1:8080/api/v1/foto',
+            urlApiAddress: 'http://127.0.0.1:8080/api/v1'
         }
     },
     methods: {
@@ -29,6 +32,27 @@ export default {
                     console.log(error);
                 });
         },
+        sendCommento(){
+            axios.post(this.urlApiAddress + `/${this.$route.params.id}/commento`, {
+                messaggio : this.messaggio
+            })
+                .then((response) => {
+                    console.log(response)
+
+                    this.messaggio = ''
+                    
+                })
+                .catch(errors => {
+
+                    console.log(errors)
+
+                    console.log(errors.response.data.errors)
+
+                    this.errors = errors.response.data.errors
+
+
+                })
+        }
 
     },
     created() {
@@ -41,6 +65,21 @@ export default {
     <h1>prova</h1>
     <FotoComp  :titolo="foto.titolo" :id="foto.id" :descrizione="foto.descrizione"
         :url="foto.url" :categorie="foto.categorie" :show="true" />
+    
+    <div class="container">
+        <div class="alert alert-danger mt-2" v-if="errors.length != 0">
+                <p v-for="error in errors" class="mb-0">{{ error }}</p>
+            </div>
+         <div class="mb-3">
+                <label for="messaggio" class="form-label">messaggio:</label>
+                <textarea class="form-control" id="messaggio" rows="15" v-model="messaggio"></textarea>
+
+
+
+            </div>
+            <button type="submit" @click="sendCommento" class="btn btn-primary">Invia</button>
+    </div>
+
 </template>
 
 <style lang="scss"></style>
